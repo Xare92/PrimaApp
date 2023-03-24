32d1f2fba9eb92ab88a81aad5581b5ec0174371f
Antonio Magliari <magliari.antonio92@gmail.com>
2023-03-24 12:26:30 +0100

    apptest

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+# Imposta lo sfondo personalizzato
+page_bg_img = '''
+<style>
+body {
+background-image: url("pxfuel.jpg");
+background-size: cover;
+}
+</style>
+'''
+
+# Imposta lo sfondo della pagina
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title('Rocket Science App')
+
+st.video('https://www.youtube.com/watch?v=DtVBCG6ThDk')
+
+st.slider('Quante Volte Daniele ha detto "non è rocket science" oggi?,min_value=0,max_value=100')
+
+import pandas as pd
+import numpy as np
+st.write('Tabella a Caso che mostra un indice di uso di quest app')
+
+df = pd.DataFrame(
+   np.random.randn(10, 11),
+   columns=('%d' % i for i in range(11)))
+
+st.dataframe(df)   #Same as st.write(df)
+
+
+
+
